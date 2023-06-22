665a3c4509b3faa87162edbc1324107dea6e3544
Chronos426 <timmynielstim1111@gmail.com>
2023-06-22 17:56:53 +0200

    Can load image.txt now

--- Code/dataloader.m
+++ Code/dataloader.m
@@ -1,4 +1,4 @@
-function [data_set,cam_data] = dataloader
+function [data_set,cam_data,im_data] = dataloader
 %% Load the images
     % Prompt the user to select the folder containing the images
     folderPath = uigetdir('Select the folder containing the images');
@@ -20,13 +20,29 @@ function [data_set,cam_data] = dataloader
     % Display the number of images loaded
     fprintf('Total images loaded: %d\n', numel(data_set));
 
-%% Load the Camera Parameters 
+    %% Load the Camera Parameters 
 
-    [baseName, folder] = uigetfile('*.txt', 'Select a text file');
+    [baseName, folder] = uigetfile('*.txt', 'Select camera txt');
     path = fullfile(folder, baseName);
-    
     filecontent = readtable(path);
-    cam_data = table2array(filecontent(:,end-11:end)); % This line works only for the eh3 data set
+
+    k = table2array(filecontent(2,end-5:end)); 
+    K = [k(3) 0 k(5);0 k(4) k(6);0 0 1];
+    size = [k(1),k(2)];
+    cam_data = cameraParameters("K",K,"ImageSize",size);
+
+    %% Load the Image Parameters (Not sure how the format is)
+    [baseName, folder] = uigetfile('*.txt', 'Select image txt');
+    path = fullfile(folder, baseName);
+    filecontent = readtable(path);
+    sz = size(filecontent,1);
+    
+    im_data = zeros(sz,3);
+    content = table2array(filecontent(:,1:4));
+
+    for i = 1:sz
+        im_data(content(i,1),:) = content(i,2:4);
+    end
 
     toc
 
