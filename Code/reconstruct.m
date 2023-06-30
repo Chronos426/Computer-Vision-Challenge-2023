a5816768b54ddfeac4105ab3662737dd21060f7b
Xiaochuan Ma <136647535+xiaochuanma@users.noreply.github.com>
2023-06-30 02:24:31 +0200

    tested, works well

--- Code/reconstruct.m
+++ Code/reconstruct.m
@@ -1,39 +1,61 @@
-function [reconstructed_points, inlierIdx] = reconstruct(data_set, matches, cam_data)
-
-    numImages = size(data_set,1);
-
+function [world_points, inlierIdx] = reconstruct(data_set, matches, cam_data, im_data)
+    %% Get number of images
+    numImages = numel(data_set);
+    
+    %% 3D reconstruction
     % Initialize the cell array to store the 3D world points
-    worldPoints = cell(numImages, numImages);
+    world_points = cell(numImages, numImages);
     inlierIdx = cell(numImages, numImages);
     
-    cameraPose1 = rigidtform3d(eye(3), [0; 0; 0]);
-    cameraPose2 = rigidtform3d(eye(3), [0; 0; 0]);
-    
-    for i = 1:numImages-1
-        % Estimate the fundamental matrix
-        [E, inliers] = estimateEssentialMatrix(matches{i,i+1}.P1, matches{i,i+1}.P2,cam_data);
-        inlierIdx{i} = inliers;       
-        
-        % Decompose the essential matrix
-        relPose = estrelpose(E,cam_data.Intrinsics,matches{i,i+1}.P1(inliers, :), ...
-            matches{i,i+1}.P2(inliers, :));
-        
-        % Perform triangulationn
-        cameraPose1 = cameraPose2;
-        cameraPose2 = [cameraPose1.R * relPose.R, cameraPose1.Translation + relPose.Translation];
-        [worldPoints{i}, err, valid] = triangulate(matches{i,i+1}.P1(inliers, :), ...
-            matches{i,i+1}.P2(inliers, :), cameraPose1, cameraPose2);
-    end
-    
-    % Concatenate all world points into a single array
-    reconstructed_points = vertcat(worldPoints{:});
+    % Initialize waitbar
+    h = waitbar(0,'Processing images...');
 
-    % Remove points with high reprojection errors (optional)
-%     maxReprojError = 5;
-%     reconstructed_points(reprojError > maxReprojError, :) = [];
-    
-    % Visualize the 3D world points
-%     pcshow(reconstructed_points, 'VerticalAxis', 'Y', 'VerticalAxisDir', 'Down', 'MarkerSize', 500);
-%     grid on;
-%     title('3D Reconstruction');
+    totalIterations = (numImages-1)*numImages/2; % total iterations for waitbar
+    currentIteration = 0; % current iteration counter
+
+    for i = 1:numImages-1
+        for j = i+1:numImages
+        % Estimate the fundamental matrix
+        [F, inliers] = estimateFundamentalMatrix(matches{i,j}.P1, matches{i,j}.P2, 'Method', 'RANSAC', 'NumTrials', 5000, 'DistanceThreshold', 0.01);
+        inlierIdx{i,j} = inliers;
+        
+        % Compute the essential matrix
+        E = cam_data.K' * F * cam_data.K;
+        
+        % Extract the inlier points
+        inlierPoints1 = matches{i,j}.P1(inliers, :);
+        inlierPoints2 = matches{i,j}.P2(inliers, :);
+
+        % Estimate the relative pose
+        relPose = estrelpose(E, cam_data.Intrinsics, inlierPoints1, inlierPoints2);
+
+        % Get projection matrices for the two images
+        cameraPose1 = rigidtform3d(eye(3), [0; 0; 0]);
+        cameraPose2 = rigidtform3d(eye(3), [0; 0; 0]);
+        cameraPose2.R = cameraPose1.R * relPose(1,1).R;
+        cameraPose2.Translation = cameraPose1.Translation + relPose(1,1).Translation;
+
+
+        % Perform triangulationn
+        [world_points{i,j}, err, valid] = triangulate(matches{i,j}.P1(inliers, :), matches{i,j}.P2(inliers, :), cameraPose1.A(1:3,:), cameraPose2.A(1:3,:));
+        % Update waitbar
+        fprintf('Loop completed with i = %d and j = %d\n', i, j);
+        currentIteration = currentIteration + 1;
+        waitbar(currentIteration/totalIterations, h, sprintf('Processing images... %d/%d done', currentIteration, totalIterations));
+        
+        end
+    end
+    % Close waitbar
+    close(h);
+    % %% Concatenate all world points into a single array
+    % reconstructed_points = vertcat(world_points{:});
+    % 
+    % % Remove points with high reprojection errors (optional)
+    % % maxReprojError = 5;
+    % % reconstructed_points(reprojError > maxReprojError, :) = [];
+    % 
+    % % Visualize the 3D world points
+    % pcshow(reconstructed_points, 'VerticalAxis', 'Y', 'VerticalAxisDir', 'Down', 'MarkerSize', 500);
+    % grid on;
+    % title('3D Reconstruction');
 end
